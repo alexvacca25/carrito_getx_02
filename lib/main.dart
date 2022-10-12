89f450d37a9d88a945b4e66a38904e8e8554942d
Alex <ronaldalexandervacca@unicesar.edu.co>
2022-10-12 13:29:29 -0500

    Carrito 02

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:carrito_getx_02/domain/controller/controlcarrito.dart';
+import 'package:carrito_getx_02/ui/app.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  Get.put(ControlCompras());
+  runApp(const App());
+}
