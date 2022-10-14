2a8894e8337d07f9ada04510e971f6586c994b53
Alex <ronaldalexandervacca@unicesar.edu.co>
2022-10-14 14:02:29 -0500

    Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:carrito_getx_02/domain/controller/controlcarrito.dart';
+import 'package:carrito_getx_02/domain/controller/controluser.dart';
 import 'package:carrito_getx_02/ui/app.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
   Get.put(ControlCompras());
+  Get.put(ControladorUser());
   runApp(const App());
 }
--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,3 +1,5 @@
+import 'package:carrito_getx_02/ui/auth/login.dart';
+import 'package:carrito_getx_02/ui/pages/carrito.dart';
 import 'package:carrito_getx_02/ui/pages/productos.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,10 +9,15 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       title: 'Carrito de Compras',
       debugShowCheckedModeBanner: false,
-      home: Productos(),
+      routes: {
+        '/login': (context) => const Login(),
+        '/productos': (context) => const Productos(),
+        '/carrito': (context) => const Carrito(),
+      },
+      home: const Login(),
     );
   }
 }
